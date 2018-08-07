f1a0370e94918d14a9c4b8787ca0438d68d962d7
Frank Gutierrez <dearfrankg@gmail.com>
2018-08-06 21:01:57 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
+import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
+const Nav = () => (
+  <div className="app">
+    <UserAvatar size="small" />
+  </div>
+);
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
-
+const App = () => <div className="app">foo</div>;
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
